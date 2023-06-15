9c716fb7e018555d430ab92159fffa126cb2e974
Ruthvikabhimavarapu <ruthvika1415@gmail.com>
2023-06-15 11:44:08 -0400

    settersandgetters

--- src/com/Person.java
+++ src/com/Person.java
@@ -1,12 +1,48 @@
 package com;
 
 public class Person {
+	
+	private String personName;
+	private int personAge;
+	
+	public Person(String personName, int personAge)
+	{
+		this.personName = personName;
+		this.personAge = personAge;
+	}
+	
+	public String getPersonName()
+	{
+		return personName;
+	}
+	
+	public int getPersonAge()
+	{
+		return personAge;
+		
+	}
+	
+	
+	
+	
+
+	public void setPersonName(String personName) {
+		this.personName = personName;
+	}
+
+	public void setPersonAge(int personAge) {
+		this.personAge = personAge;
+	}
 
 	public static void main(String[] args) {
-	  String personName;
-	  int personAge;
-	  String personPhnnum;
-	  String personAddress;
+		 
+		Person person = new Person("Nagraju",25);
+				
+				System.out.println("Name:" + person.getPersonName());
+		        System.out.println("Age" + person.getPersonAge());
+		        
+				
+	  
 
 	}
 
